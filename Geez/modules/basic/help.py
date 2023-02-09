b47cd62d312b5e610b5b136fed7455dbcbe577eb
Wiki W <99019030+Wiki28@users.noreply.github.com>
2023-02-09 12:59:03 +0800

    Update help.py

--- Geez/modules/basic/help.py
+++ Geez/modules/basic/help.py
@@ -46,13 +46,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "Geez Pyro Plugins"
+            ac.title = "Wiki PyroBot Plugins"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @GeezRam >< @UserbotCh •",
+                f"```{str(ac)}```\n• @AboutWiki •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -66,7 +66,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"**Help For {str(help_arg).upper()}**\n\n"
             for x in commands:
                 this_command += f"**Command:** `{str(x)}`\n  ∟ **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @GeezRam >< @UserbotCh"
+            this_command += "© @AboutWiki"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -88,12 +88,12 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "Geez Pyro Plugins"
+        ac.title = "Wiki PyroBot Plugins"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @GeezSupport >< @ramsupportt •"
+            message, f"```{str(ac)}```\n• @AboutWiki •"
         )
         await message.reply(
             f"**Usage**:`{cmds}help broadcast` **untuk melihat detail modul**"
@@ -105,7 +105,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"**Help For {str(help_arg).upper()}**\n\n"
             for x in commands:
                 this_command += f"**Command:** `{str(x)}`\n  ∟ **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @GeezSupport >< @ramsupportt"
+            this_command += "© @AboutWiki"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
