831ab41d24f1c83b83eb563a7ad6f3dc65df4ae0
Wiki W <99019030+Wiki28@users.noreply.github.com>
2023-02-09 23:37:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv(".env")
 API_ID = int(getenv("API_ID", "8529843")) #optional
 API_HASH = getenv("API_HASH", "6e06fb8f7b42fb33821f272597321bc1") #optional
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = getenv("OWNER_ID", "8529843")
+OWNER_ID = getenv("OWNER_ID", "")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://not:not@cluster0.8rcyhkd.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("BOT_TOKEN", "5955445604:AAHfetV9cxbp9s9YCCJU3GxKgYLwuZ2lvb8")
 ALIVE_PIC = getenv("ALIVE_PIC")
@@ -23,7 +23,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 BRANCH = getenv("BRANCH", "main") #don't change
 CMD_HNDLR = getenv("CMD_HNDLR", ",") 
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
-STRING_SESSION2 = getenv("STRING_SESSION2", "BQA_l0Dyk6skulbHhf_ufg05F58Pkf6jes6rxdO3wSniuUC8dLTwc96bzzuCxqXCJ0GmWso_U6NWE0ds-1M9lIsX0fSMjLAtEQc_qO9VeAuqolILqV0cCPmS6APBPlCtaOn1IS7bUl0gf9JthvMlz8EEDU1M3bP8JlTu8hw1w3CGP0ZPZnzlOdnQuIPi4Am21lqcpr24N-S_5qMvaPcdxuqBxbNQqXnrecnM7z3nhBdbkC5jEkxoUUw8eyqac-K9WpaZ-Lb6eC7Hd-oWPgmD4utZBz8iDClcDXfWJoJYI7sn6wWsmCj44sjFGm7cFb-yqjPYQUrfL41n2aq5bIqKzG4FdgzYWwA")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
 STRING_SESSION5 = getenv("STRING_SESSION5", "")
