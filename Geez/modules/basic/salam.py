65a9273ed8bb3eacb330d81c37f4d0875e97c310
Wiki W <99019030+Wiki28@users.noreply.github.com>
2023-02-10 12:50:32 +0800

    Update salam.py

--- Geez/modules/basic/salam.py
+++ Geez/modules/basic/salam.py
@@ -71,6 +71,37 @@ async def salamarab(client: Client, message: Message):
     await asyncio.sleep(2)
     await xx.edit("السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ")
 
+
+@Client.on_message(filters.command("vr", cmds) & filters.me)
+async def salamarab(client: Client, message: Message):
+    xx = await edit_or_reply(message, "**OOOO**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**INI YANG VIRTUAL**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**YANG KATANYA SAYANG BANGET**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**TAPI TETEP AJA DI TINGGAL**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**NI INGET**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**TANGANNYA AJA GA BISA DI PEGANG**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**APALAGI OMONGANNYA**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**BHAHAHAHA**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**KASIAN MANA MASIH MUDA**")
+
+@Client.on_message(filters.command("gbn", cmds) & filters.me)
+async def globalfake(client: Client, message: Message):
+    user_id, reason = await extract_user_and_reason(message, sender_chat=True)
+    user = await client.get_users(user_id)
+    xx = await edit_or_reply(message, f"Memulai Proses Global Banned [{user.first_name}](tg://user?id={user.id})")
+    await asyncio.sleep(3)
+    await xx.edit("`Gbanning....`")
+    await asyncio.sleep(5)
+    await xx.edit(f"**\\\ Berhasil Gbanning //** \nFirst Name : [{user.first_name}](tg://user?id={user.id})\nReason : {reason}")
+
 add_command_help(
     "salam",
     [
@@ -78,5 +109,6 @@ add_command_help(
         [f"{cmds}pe", "Assalamualaikum Warahmatullahi Wabarakatuh."],
         [f"{cmds}l", "Wa'alaikumsalam."],
         [f"{cmds}ass", "Assalamualaikum Bahas arab."],
+        [f"{cmds}vr", "Ngatain bocah virtualan."],
     ]
 )
