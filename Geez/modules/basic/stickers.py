f795a73680532046089eb9c01b46eeab6b2fd517
Wiki W <99019030+Wiki28@users.noreply.github.com>
2023-02-12 14:48:39 +0800

    Update stickers.py

--- Geez/modules/basic/stickers.py
+++ Geez/modules/basic/stickers.py
@@ -604,20 +604,17 @@ add_command_help(
 
 
 add_command_help(
-    "memify",
+    "sticker fun",
     [
         [f"{cmds}mmf Top Text ; Bottom Text",
             "balas ke stiker untuk membuat memify text stiker."],
-    ],
-)
-
-
-add_command_help(
-    "tiny",
-    [
-        [
-            f"{cmds}tiny [reply ke photo/sticker]",
-            "To Change the Sticker to be Small.",
-        ],
+        [f"{cmds}tiny [reply ke photo/sticker]",
+            "To Change the Sticker to be Small."],
+        [f"{cmds}txtst <pesan> atau <balas ke pesan>",
+            "merubah text jadi sticker."],
+        [f"{cmds}twitt <balas ke pesan>",
+            "mebuat stiker status twitter."],
+        [f"{cmds}toanime <balas ke photo>",
+            "membuat foto menjadi anime menggunakan ai."],
     ],
 )
